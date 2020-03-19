e52261fba06cb3bfddadba2d51cf4bfa4921edb1
mehmetunlu07 <maxbusiness24@gmail.com>
2020-03-19 07:58:13 -0500

    Class12 OOP1 revised

--- src/com/syntax/class12/OOP1.java
+++ src/com/syntax/class12/OOP1.java
@@ -3,8 +3,10 @@ package com.syntax.class12;
 public class OOP1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int x=5;
+		int y=6;
+		int z=x+y;
+		System.out.println(z);
 	}
 
 }
