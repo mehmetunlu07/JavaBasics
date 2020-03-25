eca06ed1245989c5b6701ccf916259735fb25ece
mehmetunlu07 <maxbusiness24@gmail.com>
2020-03-24 21:34:08 -0500

    class13 created

---
+++ src/com/syntax/class13/Encapsulation.java
@@ -0,0 +1,18 @@
+package com.syntax.class13;
+
+public class Encapsulation {
+
+	private String name; // private = restricted access
+
+	  // Getter
+	  public String getName() {
+	    return name;
+	  }
+
+	  // Setter
+	  public void setName(String newName) {
+	    name = newName;
+	  }
+	  
+	 
+	}
---
+++ src/com/syntax/class13/EncapsulationType.java
@@ -0,0 +1,11 @@
+package com.syntax.class13;
+
+public class EncapsulationType {
+
+	public static void main(String[] args) {
+		Encapsulation myObj = new Encapsulation();
+		
+		myObj.setName("John"); // Set the value of the name variable to "John"
+		System.out.println(myObj.getName());
+	}
+}
